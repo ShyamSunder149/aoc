cbc61dafa0e93aa4b0dae9497acbce43ce41e733
ShyamSunder149 <SubstantialCitron0@protonmail.com>
2024-08-03 22:59:07 +0530

    was trying to do part 2 in d1

--- aoc23/d1/main2.go
+++ aoc23/d1/main2.go
@@ -36,16 +36,37 @@ func main ()  {
     "nine" : "9",
     "zero" : "0",
   }
-
+  
   for _, value := range lines {
     
     numeric_string := ""
-    
+
+    flag := true
+    i := 0
+    ts := ""
+    for flag {
+      fmt.Println(ts)
+      if _, err := strconv.Atoi(string(value[i])); err == nil {
+        ts = ts[1:]
+        if _, ok := mappings[ts];  ok{
+          value = strings.Replace(value, ts, mappings[ts], -1)
+          flag = false 
+        } else { ts = ts[1:] }
+      } else {
+        ts += string(value[i])
+        if _, ok := mappings[ts]; ok {
+        value = strings.Replace(value, ts, mappings[ts], -1)
+          flag = false
+        }
+        i += 1
+      }
+    } 
+
     for k, v := range mappings {
-      fmt.Println(k)
       value = strings.Replace(value, k, v, -1)
     }
-
+    
+    fmt.Println(value)  
     for _, character := range value {
       if _, err := strconv.Atoi(string(character)); err == nil  {
         numeric_string = numeric_string + string(character)
@@ -54,7 +75,6 @@ func main ()  {
     
     n := len(numeric_string)
     numeric_string = string(numeric_string[0]) + string(numeric_string[n-1])
-
     number, err := strconv.Atoi(numeric_string)
     if err != nil {
       log.Fatal(err)
